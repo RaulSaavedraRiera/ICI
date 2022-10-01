30a8233404da7bc67e8f5ad49df646bdc2b65c63
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2022-10-01 11:41:34 +0200

    Fantasma sin predecir movimientos y sin huida

---
+++ src/es/ucm/fdi/ici/c2223/practica1/grupo12/GhostsP1.java
@@ -0,0 +1,180 @@
+package es.ucm.fdi.ici.c2223.practica1.grupo12;
+
+import java.awt.Color;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Random;
+
+import pacman.controllers.GhostController;
+import pacman.game.Constants;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
+import pacman.game.Game;
+import pacman.game.GameView;
+
+public class GhostsP1 extends GhostController {
+	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
+	private MOVE[] allMoves = MOVE.values();
+	private Random rnd = new Random();
+	private int range = 50;
+	private int pacmanMaxDistanceToPill = 40;
+
+	private int[] currentGhostDest = new int[] {-1, -1, -1, -1};
+
+	private JunctionManager junctionMan = new JunctionManager();
+	
+	@Override
+	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+		
+		// limpiamos movimientos
+		moves.clear();
+		
+		junctionMan.update();
+		
+		// para cada uno de los fantasmas
+		for (Constants.GHOST ghostType : Constants.GHOST.values()) {
+			if (game.doesGhostRequireAction(ghostType)) {
+
+				int ghostNode = game.getGhostCurrentNodeIndex(ghostType);
+				MOVE ghostLastMoveMade = game.getGhostLastMoveMade(ghostType);
+				int pacmanNode = game.getPacmanCurrentNodeIndex();
+				MOVE pacmanLastMoveMade = game.getPacmanLastMoveMade();
+
+				int dist = game.getShortestPathDistance(ghostNode, pacmanNode);
+
+				// fuera de rango
+				if (dist > range) {
+
+					// si actualmente el fantasma tiene un objetivo
+					if (currentGhostDest[ghostType.ordinal()] != -1) {
+						
+						GameView.addLines(game, Color.GREEN, ghostNode, currentGhostDest[ghostType.ordinal()]);
+						
+						MOVE move = game.getApproximateNextMoveTowardsTarget(ghostNode, currentGhostDest[ghostType.ordinal()], ghostLastMoveMade, DM.PATH);
+
+						moves.put(ghostType, move);
+						
+						//si ha llegado al destino cambia el objetivo
+						if (game.getShortestPathDistance(ghostNode, currentGhostDest[ghostType.ordinal()]) <= 20) {
+							currentGhostDest[ghostType.ordinal()] = -1;
+						}
+
+						break;
+					}
+
+					// 2 van a por pacman evitándose, marcan en el array de marcas las intersecciones
+					if (ghostType == GHOST.BLINKY || ghostType == GHOST.PINKY) {
+						MOVE move = game.getApproximateNextMoveTowardsTarget(ghostNode, pacmanNode, ghostLastMoveMade,
+								DM.PATH);
+						
+						move = junctionMan.getNextAvailableMove(ghostNode, move, ghostLastMoveMade);
+
+						moves.put(ghostType, move);
+					}
+
+					// 2 van a una zona cercana a pacman sin entrar en
+					// elige un nodo aleatorio del mapa (que sea interseccion) y mira si la distancia hasta pacman es menos
+					// que 2 veces el rango
+					else {
+						Boolean close = false;
+
+						int randomNode = -1;
+						
+						int[] junctions = game.getJunctionIndices();
+
+						while (!close) {
+							
+							int randomIndex = rnd.nextInt(junctions.length);
+							randomNode = junctions[randomIndex];
+
+							int distance = game.getShortestPathDistance(randomNode, pacmanNode);
+
+							if (distance < range * 2)
+								close = true;
+						}
+						
+						System.out.println("Nodo random " + ghostType + ": " + randomNode);
+
+						currentGhostDest[ghostType.ordinal()] = randomNode;
+
+						MOVE move = game.getApproximateNextMoveTowardsTarget(ghostNode, randomNode, ghostLastMoveMade,
+								DM.PATH);
+
+						move = junctionMan.getNextAvailableMove(ghostNode, move, ghostLastMoveMade);
+						
+						moves.put(ghostType, move);
+					}
+				}
+
+				// pacman en rango
+				else {
+
+					GameView.addLines(game, Color.RED, pacmanNode, ghostNode);
+					
+					// si habia un objetivo, pero ha entrado en rango, quita dicho objetivo
+					if (currentGhostDest[ghostType.ordinal()] != -1)
+						currentGhostDest[ghostType.ordinal()] = -1;
+
+					int closestPP = getClosestPowerPill(game, pacmanNode, pacmanLastMoveMade);
+
+					int pacmanDistToPP = game.getShortestPathDistance(pacmanNode, closestPP, pacmanLastMoveMade);
+
+					// si pacman esta cerca de la PP
+					if (pacmanDistToPP < pacmanMaxDistanceToPill) {
+						int ghostDistanceToPP = game.getShortestPathDistance(ghostNode, closestPP, pacmanLastMoveMade);
+
+						// si el fantasma llega antes a la PP va a por ella
+						if (ghostDistanceToPP < pacmanDistToPP) {
+							MOVE move = game.getApproximateNextMoveTowardsTarget(ghostNode, closestPP,
+									ghostLastMoveMade, DM.PATH);
+
+							moves.put(ghostType, move);
+						}
+
+						// si no huye
+						else {
+							MOVE move = game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
+									ghostLastMoveMade, DM.PATH);
+
+							moves.put(ghostType, move);
+						}
+					}
+
+					// Si no esta cerca de PP va hacia donde se dirige Pacman
+					else {
+						MOVE move = game.getApproximateNextMoveTowardsTarget(ghostNode, pacmanNode, ghostLastMoveMade,
+								DM.PATH);
+						
+						move = junctionMan.getNextAvailableMove(ghostNode, move, ghostLastMoveMade);
+
+						moves.put(ghostType, move);
+					}
+				}
+			}
+		}
+
+		return moves;
+	}
+
+	// Devuelve la power pill mas cercana al nodo dado, teniendo en cuenta el ultimo
+	// movimiento realizado
+	private int getClosestPowerPill(Game game, int nodeFrom, MOVE lastMove) {
+
+		int[] pPills = game.getActivePowerPillsIndices();
+
+		int minDistPillNode = 0;
+		int minDist = Integer.MAX_VALUE;
+		for (int pillNode : pPills) {
+			int dist = game.getShortestPathDistance(nodeFrom, pillNode, lastMove);
+
+			if (dist < minDist) {
+				minDist = dist;
+				minDistPillNode = pillNode;
+			}
+		}
+
+		return minDistPillNode;
+	}
+
+}
---
+++ src/es/ucm/fdi/ici/c2223/practica1/grupo12/JunctionManager.java
@@ -0,0 +1,104 @@
+package es.ucm.fdi.ici.c2223.practica1.grupo12;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import pacman.game.Constants;
+import pacman.game.Constants.MOVE;
+
+/**
+ * @author Rodrigo
+ *
+ */
+public final class JunctionManager {
+	
+	private final int TICKS_TO_CLEAR = 8;
+	 
+	private HashMap<Integer, HashMap<MOVE, Integer>> junctionMarks;
+	
+	/**
+	 * Iterates through the map reducing the remaining ticks of each move.
+	 * If the remaining ticks reaches 0 removes the entry from the map.
+	 */
+	public void update() 
+	{
+		for (HashMap<MOVE, Integer> junctions : junctionMarks.values()) 
+		{
+			for (Entry<MOVE, Integer> entry : junctions.entrySet()) 
+			{
+				int ticks = entry.getValue();
+				
+				entry.setValue(ticks - 1);
+				
+				if (ticks == 0) 
+				{
+					junctions.remove(entry.getKey());
+				}
+	        }
+        }
+	}
+	
+	public boolean isDirectionAvailable(int junction, MOVE move) 
+	{
+		if (junctionMarks.containsKey(junction)) 
+		{
+			if (junctionMarks.get(junction).containsKey(move)) return false;
+		}
+		
+		return true;
+	}
+	
+	public void markDirection(int junction, MOVE move) 
+	{
+		if (junctionMarks.containsKey(junction)) 
+		{
+			junctionMarks.get(junction).put(move, TICKS_TO_CLEAR);
+			
+			junctionMarks.put(junction, junctionMarks.get(junction));
+		}
+		
+		else 
+		{
+			HashMap<MOVE, Integer> map = new HashMap<MOVE, Integer>();
+			map.put(move, TICKS_TO_CLEAR);
+			
+			junctionMarks.put(junction, map);
+		}
+	}
+	
+	/**
+	 * Method that searches for the first direction available in a junction and adds it to the mark hashmap
+	 * @param junction Juntion where to check directions
+	 * @param moveToCheck Move to check if is available
+	 * @param lastMoveMade LastMove made by the entity to check
+	 * @return the first available move
+	 */
+	public MOVE getNextAvailableMove(int junction, MOVE moveToCheck, MOVE lastMoveMade) 
+	{
+		if (isDirectionAvailable(junction, moveToCheck)) 
+		{
+			markDirection(junction, moveToCheck);
+			
+			return moveToCheck;
+		}
+		
+		MOVE move = moveToCheck;
+		
+		for (MOVE m : Constants.MOVE.values()) 
+		{
+			if (m != move && m.opposite() != lastMoveMade && isDirectionAvailable(junction, m)) 
+			{
+				move = m;
+			}
+		}
+		
+		markDirection(junction, move);
+		
+		return move;
+	}
+
+	public JunctionManager() {
+		super();
+		this.junctionMarks = new HashMap<Integer, HashMap<MOVE, Integer>>();
+	}
+}
