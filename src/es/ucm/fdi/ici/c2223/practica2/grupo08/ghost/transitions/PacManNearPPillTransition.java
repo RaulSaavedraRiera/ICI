45488fe79bd80defaca84cbf5eaf39043d81e220
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2022-10-26 15:18:35 +0200

    PAQUETES

---
+++ src/es/ucm/fdi/ici/c2223/practica2/grupo08/ghost/transitions/PacManNearPPillTransition.java
@@ -0,0 +1,30 @@
+package es.ucm.fdi.ici.c2223.practica2.grupo08.ghost.transitions;
+
+import es.ucm.fdi.ici.Input;
+import es.ucm.fdi.ici.c2223.practica2.grupo08.ghosts.GhostsInput;
+import es.ucm.fdi.ici.fsm.Transition;
+
+public class PacManNearPPillTransition implements Transition {
+
+	public static double thresold = 30;
+	
+	public PacManNearPPillTransition() {
+		super();
+	}
+
+
+	@Override
+	public boolean evaluate(Input in) {
+		GhostsInput input = (GhostsInput) in;
+		return input.getMinPacmanDistancePPill() < thresold;
+	}
+
+
+	@Override
+	public String toString() {
+		return "MsPacman near PPill";
+	}
+
+	
+	
+}
