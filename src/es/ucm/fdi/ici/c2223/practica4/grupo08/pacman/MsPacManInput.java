638985bbbf1ac03ae2c734654776a94c6f38a9fa
RaulSaavedraRiera <rsaave02@ucm.es>
2022-11-25 15:23:26 +0100

    cosass

--- src/es/ucm/fdi/ici/c2223/practica4/grupo08/pacman/MsPacManInput.java
+++ src/es/ucm/fdi/ici/c2223/practica4/grupo08/pacman/MsPacManInput.java
@@ -1,27 +1,52 @@
 package es.ucm.fdi.ici.c2223.practica4.grupo08.pacman;
 
-import java.util.Map;
+import java.util.HashMap;
 
 import es.ucm.fdi.ici.fuzzy.FuzzyInput;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
 import pacman.game.Game;
 
-public class MsPacManInput extends FuzzyInput{
+public class MsPacManInput extends FuzzyInput {
 
+	private double[] distance;
+	
+	
 	public MsPacManInput(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public Map<String, Double> getFuzzyValues() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	@Override
 	public void parseInput() {
-		// TODO Auto-generated method stub
+
+		distance = new double[] {-1,-1,-1,-1};
+
 		
+		for(GHOST g: GHOST.values()) {
+			int index = g.ordinal();
+			int pos = game.getGhostCurrentNodeIndex(g);
+			if(pos != -1) {
+				distance[index] = game.getDistance(game.getPacmanCurrentNodeIndex(), pos, DM.PATH);
+			}
+			else
+				distance[index] = -1;
+		}
+	}
+	
+	public boolean isVisible(GHOST ghost)
+	{
+		return distance[ghost.ordinal()]!=-1;
+	}
+	
+	
+
+	@Override
+	public HashMap<String, Double> getFuzzyValues() {
+		HashMap<String,Double> vars = new HashMap<String,Double>();
+		for(GHOST g: GHOST.values()) {
+			vars.put(g.name()+"distance",   distance[g.ordinal()]);
+		}
+		return vars;
 	}
 
 }
