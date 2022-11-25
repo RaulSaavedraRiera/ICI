2a8fb659b8bd8305e22f568a7dc81b37b5101410
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2022-11-25 23:53:26 +0100

    ghost fuzzy mem

--- src/es/ucm/fdi/ici/c2223/practica4/grupo08/ghosts/GhostsFuzzyMemory.java
+++ src/es/ucm/fdi/ici/c2223/practica4/grupo08/ghosts/GhostsFuzzyMemory.java
@@ -1,5 +1,33 @@
 package es.ucm.fdi.ici.c2223.practica4.grupo08.ghosts;
 
+import java.util.HashMap;
+
+import pacman.game.Constants.GHOST;
+
 public class GhostsFuzzyMemory {
 
+	private int pacmanLastPosition;
+	private int pacmanPosConfidence;
+	private HashMap<Integer, Boolean> activePP;
+	
+	private HashMap<String, Double> mem;
+	
+	public void getInput(GhostsInput input)
+	{
+		for(GHOST g: GHOST.values()) {
+			double conf = pacmanPosConfidence;
+			if(input.isVisible(g))
+				conf = 100;
+			else
+				conf = Double.max(0, conf-5);
+			
+			mem.put("PacmanPosConfidence", conf);	
+			mem.put("PacmanPos", (double)pacmanLastPosition);
+		}
+
+	}
+	
+	public HashMap<String, Double> getFuzzyValues() {
+		return mem;
+	}
 }
--- src/es/ucm/fdi/ici/c2223/practica4/grupo08/ghosts/GhostsInput.java
+++ src/es/ucm/fdi/ici/c2223/practica4/grupo08/ghosts/GhostsInput.java
@@ -1,15 +1,22 @@
 package es.ucm.fdi.ici.c2223.practica4.grupo08.ghosts;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import es.ucm.fdi.ici.fuzzy.FuzzyInput;
 import pacman.game.Game;
+import pacman.game.Constants.GHOST;
 
 public class GhostsInput extends FuzzyInput{
 
 	public GhostsInput(Game game) {
 		super(game);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean isPacmanVisible()
+	{
+		return game.getPacmanCurrentNodeIndex() != -1;
 	}
 
 	@Override
