b6b5369a6bc5577b21ae8870bd9ea0e18352d1ee
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2022-11-28 19:59:08 +0100

    input ghost distance

--- src/es/ucm/fdi/ici/c2223/practica4/grupo08/ghosts/GhostsInput.java
+++ src/es/ucm/fdi/ici/c2223/practica4/grupo08/ghosts/GhostsInput.java
@@ -5,30 +5,45 @@ import java.util.Map;
 
 import es.ucm.fdi.ici.fuzzy.FuzzyInput;
 import pacman.game.Game;
+import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 
-public class GhostsInput extends FuzzyInput{
+public class GhostsInput extends FuzzyInput {
+
+	private int[] distance;
 
 	public GhostsInput(Game game) {
 		super(game);
 		// TODO Auto-generated constructor stub
 	}
-	
-	public boolean isPacmanVisible()
-	{
+
+	public boolean isPacmanVisible() {
 		return game.getPacmanCurrentNodeIndex() != -1;
 	}
 
 	@Override
 	public Map<String, Double> getFuzzyValues() {
 		// TODO Auto-generated method stub
-		return null;
+
+		HashMap<String, Double> vars = new HashMap<String, Double>();
+		for (GHOST g : GHOST.values()) {
+			vars.put(g.name() + "distance", (double) distance[g.ordinal()]);
+		}
+		return vars;
 	}
 
 	@Override
 	public void parseInput() {
 		// TODO Auto-generated method stub
-		
+		distance = new int[] { -1, -1, -1, -1 };
+
+		for (GHOST g : GHOST.values()) {
+			int pos = game.getPacmanCurrentNodeIndex();
+			if (pos != -1) {
+				distance[g.ordinal()] = game.getShortestPathDistance(game.getGhostCurrentNodeIndex(g), pos,
+						game.getGhostLastMoveMade(g));
+			}
+		}
 	}
 
 }
