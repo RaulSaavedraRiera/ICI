d687242e7c3b7d1de338cfbcb9c15f2b4755b28d
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2022-12-16 14:47:26 +0100

    pract 5

---
+++ src/es/ucm/fdi/ici/c2223/practica5/grupo08/ghosts/GhostsResult.java
@@ -0,0 +1,39 @@
+package es.ucm.fdi.ici.c2223.practica5.grupo08.ghosts;
+
+import es.ucm.fdi.gaia.jcolibri.cbrcore.Attribute;
+import es.ucm.fdi.gaia.jcolibri.cbrcore.CaseComponent;
+
+public class GhostsResult implements CaseComponent {
+
+	Integer id;
+	Integer score;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	@Override
+	public Attribute getIdAttribute() {
+		return new Attribute("id", GhostsResult.class);
+	}
+	
+	@Override
+	public String toString() {
+		return "MsPacManResult [id=" + id + ", score=" + score + "]";
+	} 
+	
+	
+
+}
